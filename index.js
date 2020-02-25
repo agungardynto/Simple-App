7ff1689d256fd422c848ff2183ba4204d89bc262
Agung Ardiyanto <agungardynto.com>
2020-02-25 11:05:02 +0700

    Add dotenv

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const database = require('nedb');
 const favicon = require('serve-favicon');
 const path = require('path');
+require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 3000;
